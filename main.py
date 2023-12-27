238ed32510be9fc46a806fb1212914d7eb94b255
GregS-07 <gregsternik07@gmail.com>
2023-12-27 15:50:31 +0000

    Type hints & handling language inputs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas as pd 
 import PySimpleGUI as psg
 import matplotlib.pyplot as plt 
+from typing import List
 
 languageList = [
     "Abap", "Ada", "C/C++", "C#", "Cobol", "Dart", "Delphi/Pascal", "Go", "Groovy",
-    "Haskell", "Java", "Javascript", "Julia", "Kotlin", "Lua", "Matlab", "Objective-C",
-    "Perl", "Php", "Powershell", "Python", "R", "Ruby", "Rust", "Scala", "Swift",
-    "TypeScript", "VBA", "Visual basic"
+    "Haskell", "Java", "JavaScript", "Julia", "Kotlin", "Lua", "Matlab", "Objective-C",
+    "Perl", "PHP", "Powershell", "Python", "R", "Ruby", "Rust", "Scala", "Swift",
+    "TypeScript", "VBA", "Visual Basic"
 ]
+
 #GUI setup
 layout = [
     [psg.Text("Programming Language Popularity")],
@@ -34,7 +36,7 @@ def plotGraph(langs, start, end):
     plt.legend()
     plt.show()
 
-def errorPopup(err):
+def errorPopup(err: str):
     psg.popup_error(f"There has been an error. {err}")
 
 while True:
@@ -46,10 +48,30 @@ while True:
         error = False
         langs = values["-LANGS-"].split(",")
         langs = [lang.strip().capitalize() for lang in langs]
+        def replaceArr(langs: List[str], replacementString: str, caseString: str) -> List[str]:
+            return list(map(lambda x: replacementString if x == caseString else x, langs))
         for lang in langs:
             if lang not in languageList:
-                errorPopup(f"{lang} was not found in the dataset.")
-                error = True
+                match lang:
+                    case "C":
+                        langs = replaceArr(langs, "C/C++", "C")
+                    case "C++":
+                        langs = replaceArr(langs,"C/C++","C++")
+                    case "Pascal":
+                        langs = replaceArr(langs,"Delphi/Pascal","Pascal")
+                    case "Delphi":
+                        langs = replaceArr(langs,"Delphi/Pascal","Delphi")
+                    case "Javascript":
+                        langs = replaceArr(langs,"JavaScript","Javascript")
+                    case "Js":
+                        langs = replaceArr(langs,"Javascript","Js")
+                    case "Php":
+                        langs = replaceArr(langs,"PHP","Php")
+                    case "Visual basic":
+                        langs = replaceArr(langs,"Visual Basic","Visual basic")
+                    case _:
+                        errorPopup(f"{lang} was not found in the dataset.")
+                        error = True
         
         startDate = values["-START-"]
         try:
